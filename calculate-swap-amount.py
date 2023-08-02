3c97e306f820429c68eea39e8093e0193535980f
mrowna <81671716+mrowna@users.noreply.github.com>
2023-08-02 15:12:21 +0700

    Create calculate-swap-amount.py

---
+++ calculate-swap-amount.py
@@ -0,0 +1,48 @@
+def calculate_swap(eth_balance, usd_balance, target_eth_ratio, min_eth_balance_after_swap=10):
+   
+    total_balance = eth_balance + usd_balance
+    min_ratio_ETH = (MIN_SWAP_VALUE + min_eth_balance_after_swap) / total_balance
+    max_ratio_ETH = (MAX_SWAP_VALUE + min_eth_balance_after_swap) / total_balance
+
+    current_eth_ratio = eth_balance / total_balance
+    print(f"ETH Ratio: {round(current_eth_ratio, 2)}")
+    swap_direction = "ETH->USD"
+
+    if current_eth_ratio >= target_eth_ratio:
+        # Swap ETH to USD
+        swap_direction = "ETH->USD"
+        max_randomable = eth_balance - min_eth_balance_after_swap
+        print(f"Max randomable ETH: {max_randomable}")
+
+        if max_randomable < MIN_SWAP_VALUE:
+            print("Swap amount ETH is less than minimum swap value --> CHANGE DIRECTION")
+            swap_direction = "USD->ETH"
+            max_randomable = usd_balance
+    else:
+        # Swap USD to ETH
+        swap_direction = "USD->ETH"
+        max_randomable = usd_balance #min(usd_balance, MAX_SWAP_VALUE)
+        print(f"Max randomable USD: {max_randomable}")
+
+        if max_randomable < MIN_SWAP_VALUE:
+            print("Swap amount USD is less than minimum swap value --> CHANGE DIRECTION")
+            # import pdb
+            # pdb.set_trace()
+            swap_direction = "ETH->USD"
+            max_randomable = eth_balance - min_eth_balance_after_swap
+            
+    max_random_swap_value = min(max_randomable, MAX_SWAP_VALUE)
+    print(f"Max randomable value: {max_random_swap_value}")
+    
+    swap_amount = random.randint(MIN_SWAP_VALUE, max_random_swap_value)
+
+    if swap_direction == "ETH->USD":
+        print(f"Swap {swap_amount}$ ETH -> USD")
+        eth_balance -= swap_amount
+        usd_balance += swap_amount
+    else:
+        print(f"Swap {swap_amount}$ USD -> ETH")
+        eth_balance += swap_amount
+        usd_balance -= swap_amount
+
+    return eth_balance, usd_balance, swap_amount, swap_direction
